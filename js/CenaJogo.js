787f1a77672bb0b52a755b76bf390b7005ca98af
Brysah Vidal <brysahvidal.rodrigues@estudante.ufjf.br>
2022-02-13 23:35:39 -0300

    Adiciona ganho de velocidade a cada duas moedas

--- js/CenaJogo.js
+++ js/CenaJogo.js
@@ -15,31 +15,33 @@ export default class CenaJogo extends Cena {
             this.assets.play("waka");
             this.aRemover.push(b);
             this.vidas--;
+            this.adicionar(new Sprite({ x: 201, y: 70 , color: "red", controlar: this.perseguePC, tags: ["enemy"] }));
             
         }
         }
         else if (a.tags.has("pc") && b.tags.has("coin")) {
-            if(this.score == 100)
+            if(this.score == 9)
             {
                 this.assets.play("abertura");
                 this.game.selecionaCena("vitoria");
             }else{
-                if(this.score % 4 == 0)
+                if(this.score % 2 == 0)
                 {
                             for (let i = 0; i < this.sprites.length; i++) {
                                 if(this.sprites[i].tags.has("enemy"))
                                 {
-                                    this.sprites[i].vx += 5;
-                                    this.sprites[i].vy += 5;
+                                    this.sprites[i].vx += 7;
+                                    this.sprites[i].vy += 7;
                                 }
                                 
                             }
+                            a.vx += 5;
+                            a.vy += 5;
                 }
                 this.assets.play("moeda");
                 this.aRemover.push(b);
                 this.score++;
                
-                
             }
         }
         
@@ -75,8 +77,8 @@ export default class CenaJogo extends Cena {
 
        
         function perseguePC(dt) {            
-            this.vx = 10 * Math.sign(pc.x - this.x) - 0.2 * this.vx;
-            this.vy = 10 * Math.sign(pc.y - this.y) - 0.2 * this.vy;
+            this.vx = 20 * Math.sign(pc.x - this.x) - 0.2 * this.vx;
+            this.vy = 20 * Math.sign(pc.y - this.y) - 0.2 * this.vy;
             
         }
 
@@ -95,6 +97,13 @@ export default class CenaJogo extends Cena {
         this.adicionar(new Sprite({ x: 600, y: 140 ,w: 10 , h: 10, color: "yellow", tags: ["coin"] }));
         this.adicionar(new Sprite({ x: 360, y: 210,w: 10 , h: 10, color: "yellow", tags: ["coin"] }));
         this.adicionar(new Sprite({ x: 470, y: 300,w: 10 , h: 10, color: "yellow", tags: ["coin"] }));
+       
+        this.adicionar(new Sprite({ x: 630, y: 170 ,w: 10 , h: 10, color: "yellow", tags: ["coin"] }));
+        this.adicionar(new Sprite({ x: 390, y: 240,w: 10 , h: 10, color: "yellow", tags: ["coin"] }));
+        this.adicionar(new Sprite({ x: 470, y: 360,w: 10 , h: 10, color: "yellow", tags: ["coin"] }));
+        this.adicionar(new Sprite({ x: 420, y: 270,w: 10 , h: 10, color: "yellow", tags: ["coin"] }));
+        this.adicionar(new Sprite({ x: 210, y: 270,w: 10 , h: 10, color: "yellow", tags: ["coin"] }));
+
        
     }
     
